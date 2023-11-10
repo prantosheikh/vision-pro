47f3c4dd09262a2aad1055e085c38db956419d18
Pranto Sheikh <prantosheikh834@gmail.com>
2023-11-11 00:09:49 +0600

    page two part completed

--- script.js
+++ script.js
@@ -43,3 +43,42 @@ gsap.to("#page>video",{
       document.querySelector("#page>video").play()
   }
 })
+
+
+gsap.to("#page",{
+    scrollTrigger:{
+        trigger:`#page`,
+        start:`top top`,
+        end:`bottom top`,
+        scroller:`#main`,
+        pin:true
+    }
+})
+
+
+gsap.to("#page-bottom",{
+    scrollTrigger:{
+        trigger:`#page-bottom`,
+        start:`5% top`,
+        end:`bottom top`,
+        scroller:`#main`,
+        scrub:.5,
+    },
+    opacity:0
+})
+
+
+var tl1 = gsap.timeline({
+  scrollTrigger:{
+      trigger:`#page1`,
+      start:`top top`,
+      scrub:1,
+      scroller:`#main`,
+      pin:true
+  }
+})
+
+
+tl1.to("#page1>h1",{
+  top:`-50%`
+})
